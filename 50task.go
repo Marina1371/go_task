4134272d3aad3885ebc861992a6b14767f2907ac
Marina1371 <m.semenyuk.a@gmail.com>
2023-09-21 21:19:50 +0300

    комит50newtask

--- 50task.go
+++ 50task.go
@@ -1,18 +1,24 @@
-package main
-   
-import (
-	"crypto/md5"
-	"encoding/hex"
-   )
-func mdHashing(input string) string {
-byteInput := []byte(input)
-md5Hash := md5.Sum(byteInput)
-return hex.EncodeToString(md5Hash[:]) // by referring to it as a string
-   }
-func main ()  { 
-fmt.Println(mdHashing( input:"abcdefghijklmnopqrstuvwxyz"))
-fmt.Println(mdHashing( input:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+// Генерируем ключ
+key := []byte("MySuperSecretKey")
+
+// Генерируем случайный вектор инициализации
+iv := make([]byte, aes.BlockSize)
+if _, err := rand.Read(iv); err != nil {
+	panic(err)
 }
+
+// Создаём шифратор
+block, err := aes.NewCipher(key)
+if err != nil {
+	panic(err)
+}
+mode := cipher.NewCBCEncrypter(block, iv)
+
+// Шифруем данные
+plaintext := []byte("Hello, World!")
+ciphertext := make([]byte, len(plaintext))
+mode.CryptBlocks(ciphertext, plaintext)
+
    
 
 
